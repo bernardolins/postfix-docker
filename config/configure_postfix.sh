73d90ff88352e77c0eebedd0862991211b72dbc1
Bernardo Lins <bernardolins28@gmail.com>
2015-11-24 00:11:16 -0200

    Add support to LDAP and virtual domains

--- config/configure_postfix.sh
+++ config/configure_postfix.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DOCKER_HOST=172.17.0.1
+VIRTUAL_DOMAINS=/etc/postfix/virtual_domains
+LDAP_DIR=/config/ldap
 
 if [ "$MYNETWORKS" ]; then 
   echo "Setting up mynetworks with $DOCKER_HOST, $MYNETWORKS"
@@ -12,10 +14,26 @@ fi
 
 if [ "$MYDOMAIN" ]; then 
   echo "Setting up mydomain with $MYDOMAIN"
+  echo "${MYDOMAIN}               OK" > $VIRTUAL_DOMAINS
   postconf -e "mydomain=$MYDOMAIN"
 fi
 
 if [ "$MYHOSTNAME" ]; then 
   echo "Setting up mydomain with $MYHOSTNAME"
+  echo "${MYHOSTNAME}             OK" > $VIRTUAL_DOMAINS
   postconf -e "mydomain=$MYHOSTNAME"
+  postconf -e "smtp_banner=$MYHOSTNAME ESMTP"
+fi
+
+if [-a "$VIRTUAL_DOMAINS" ]; then
+  echo "Generating virtual_domains"
+  postmap hash:$VIRTUAL_DOMAINS
+fi
+
+for file in "$LDAP_DIR"
+do
+  if [ $file != $LDAP_DIR ]; then
+    postconf -e "virtual_mailbox_maps = proxy:ldap:$file"
+    break
   fi
+done 
