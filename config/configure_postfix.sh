012d84dbfe9c0171dee3970395b8c4f99a5294c9
Bernardo Lins <bernardolins28@gmail.com>
2015-11-23 23:20:23 -0200

    Fix conditional to setup mynetworks and create configuration to myhostname and mydomain using environment variables

--- config/configure_postfix.sh
+++ config/configure_postfix.sh
@@ -2,9 +2,20 @@
 
 DOCKER_HOST=172.17.0.1
 
-if [ -z "$MYNETWORKS" ]; then 
+if [ "$MYNETWORKS" ]; then 
+  echo "Setting up mynetworks with $DOCKER_HOST, $MYNETWORKS"
   postconf -e "mynetworks=$DOCKER_HOST, $MYNETWORKS"
 else
+  echo "Setting up mynetworks with $DOCKER_HOST"
   postconf -e "mynetworks=$DOCKER_HOST"
 fi
 
+if [ "$MYDOMAIN" ]; then 
+  echo "Setting up mydomain with $MYDOMAIN"
+  postconf -e "mydomain=$MYDOMAIN"
+fi
+
+if [ "$MYHOSTNAME" ]; then 
+  echo "Setting up mydomain with $MYHOSTNAME"
+  postconf -e "mydomain=$MYHOSTNAME"
+  fi
